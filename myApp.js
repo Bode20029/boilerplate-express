c64d525fd8f837ec54ff610923cf3d77d61e6b75
Jirayu Vijjakajohn <jry.bode@gmail.com>
2024-05-22 01:48:53 +0000

    absolutePath

--- myApp.js
+++ myApp.js
@@ -6,9 +6,11 @@ console.log("Hello World")
 /*app.get('/', (req, res) => {
     res.send('Hello Express')
 }) */ // Exercise 2
+absolutePath = __dirname + '/views/index.html'
 
-
-
+app.get('/', (req, res) => {
+    res.sendFile(absolutePath)
+})
 
 
 
